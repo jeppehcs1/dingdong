6b834b4d51de503806dc60718b9a07a1e6b5b4f3
jeppehcs1 <s235223@dtu.dk>
2025-07-19 22:25:24 +0200

    d

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //test
 function dingDongButton() {
-    const image = document.getElementById('dingimage');
-      if (image.style.display === 'none') {
-        image.style.display = 'block';
-      } else {
-        image.style.display = 'none';
-      }
+  const image = document.getElementById('dingimage');
+  if (image.style.display === 'none' || image.style.display === '') {
+    image.style.display = 'block';
+  } else {
+    image.style.display = 'none';
+  }
 }
