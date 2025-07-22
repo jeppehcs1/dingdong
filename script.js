13d63a91c1a3989dc3f6560ab4afe88d46496101
jeppehcs1 <s235223@dtu.dk>
2025-07-22 10:16:39 +0200

    m

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-//test
+let lastImage = "";
+
 function dingDongButton() {
   const image = document.getElementById('dingimage');
   const dingdongs = ["jake2.PNG", "rallemus.PNG", "rallemus2.png"];
+
   if (image.style.display === 'none' || image.style.display === '') {
     image.style.display = 'block';
   } else {
-    const src = "images/" + dingdongs[Math.floor(Math.random() * dingdongs.length())]
-    image.src = src;
+    let nextImage;
+    do {
+      nextImage = "images/" + dingdongs[Math.floor(Math.random() * dingdongs.length)];
+    } while (nextImage === lastImage);
+
+    lastImage = nextImage;
+    image.src = nextImage;
   }
 }
