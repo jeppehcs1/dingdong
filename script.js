759d2b9cfe5e3cf04045f91970aff2734fd40611
jeppehcs1 <s235223@dtu.dk>
2025-07-22 11:12:18 +0200

    yup

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ let lastImage = "";
 
 function dingDongButton() {
   const image = document.getElementById('dingimage');
-  const dingdongs = ["jake2.PNG", "rallemus.PNG", "rallemus2.png"];
+  const dingdongs = ["jake2.PNG", "rallemus.PNG", "rallemus2.png", "dingdong1.png", "dingdong2.jpg",
+                      "dingdong3.jpg","dingdong4.jpg","dingdong5.png","dingdong6.jpg","dingdong7.jpg",
+                      "dingdong8.png","dingdong9.png","dingdong10.png","dingdong11.png","dingdong12.png",
+                      "dingdong13.png","dingdong14.png","dingdong15.png"
+  ];
 
   if (image.style.display === 'none' || image.style.display === '') {
     image.style.display = 'block';
