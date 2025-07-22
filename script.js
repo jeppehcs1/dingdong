1e1b016704cffa841747f3a6cd26ad0b1d59b06d
jeppehcs1 <s235223@dtu.dk>
2025-07-22 10:09:45 +0200

    fix

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 //test
 function dingDongButton() {
   const image = document.getElementById('dingimage');
+  const dingdongs = ["jake2.PNG", "rallemus.PNG", "rallemus2.png"];
   if (image.style.display === 'none' || image.style.display === '') {
     image.style.display = 'block';
   } else {
-    image.style.display = 'none';
+    const src = "images/" + dingdongs[Math.floor(Math.random() * dingdongs.length())]
+    image.src = 'none';
   }
 }
