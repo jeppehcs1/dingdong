3ec4b1d5ab9a6690d33c13450b46ec37aa13f8e8
jeppehcs1 <s235223@dtu.dk>
2025-07-22 11:25:31 +0200

    d

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let lastImage = "";
 
 function dingDongButton() {
   const image = document.getElementById('dingimage');
-  const dingdongs = ["jake2.PNG", "rallemus.PNG", "rallemus2.png", "dingdong1.PNG", "dingdong2.jpg",
+  const dingdongs = ["jake2.PNG", "rallemus.PNG", "rallemus2.PNG", "dingdong1.PNG", "dingdong2.jpg",
                       "dingdong3.jpg","dingdong4.jpg","dingdong5.PNG","dingdong6.jpg","dingdong7.jpg",
                       "dingdong8.PNG","dingdong9.PNG","dingdong10.PNG","dingdong11.PNG","dingdong12.PNG",
                       "dingdong13.PNG","dingdong14.PNG","dingdong15.PNG"
